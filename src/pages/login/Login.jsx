7892512e694a09dc6b164cbcfd521e844e9563f7
Yasmin Machado <yasmin.m.silva14@aluno.senai.br>
2025-05-06 11:50:13 -0300

    FEAT: Arummando pagina de Login, adicionando componentes e responsividade

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,34 @@
+// Importação
+import Logo from "../../assets/img/logoEvents.svg"
+import Botao from "../../components/botao/Botao";
+import "./Login.css"
+// import { Link } from "react-router-dom"
+
+const Login = () => {
+  return (
+    <main className="main_login">
+      <div className="banner"></div>
+      <section className="section_login">
+        <img src={Logo} alt="Logo do Events" />
+        <form action="" className="form_login">
+          {/* <h1>Login</h1> */}
+          <div className="campos_login">
+            <div className="campo_input">
+              {/* <label htmlFor="email">Email: </label> */}
+              <input type="email" name="email" placeholder="Username"></input>
+            </div>
+            <div className="campo_input">
+              {/* <label htmlFor="senha">Senha:</label> */}
+              <input type="password" name="senha" placeholder="Password"></input>
+              {/* <Link>Esqueceu a senha?</Link> */}
+            </div>
+          </div>
+          <Botao nomeDoBotao = "Login"/>
+        </form>
+      </section>
+    </main>
+
+  );
+}
+
+export default Login;
